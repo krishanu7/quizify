5ab59ae59fff4bc6077920b80ece7b432ecea664
Krishanu Saha <krishanu@Krishanus-MacBook-Air.local>
2024-06-08 17:13:59 +0530

    React-Query-Provider

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -2,12 +2,22 @@
 import { SessionProvider } from "next-auth/react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { type ThemeProviderProps } from "next-themes/dist/types";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
-const Providers = ({ children, ...props }: ThemeProviderProps) => {
+const queryClient = new QueryClient();
+
+const Providers = ({ children }: ThemeProviderProps) => {
   return (
-    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-      <SessionProvider>{children}</SessionProvider>
-    </NextThemesProvider>
+    <QueryClientProvider client={queryClient}>
+      <NextThemesProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        <SessionProvider>{children}</SessionProvider>
+      </NextThemesProvider>
+    </QueryClientProvider>
   );
 };
 
